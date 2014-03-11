c00e7955e66f39d60089a9ff0a6197c59b1fd5a1
Igor Baryshnikov <masterj63@gmail.com>
2014-03-12 00:15:04 +0400

    +invocation of MainActivity from SplashActivity; +cancelling invocation in onDestroy(); +android:noHistory=`true` for SplashActivity

--- SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/SplashActivity.java
+++ SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/SplashActivity.java
@@ -1,13 +1,34 @@
 package mdev.master_j.simpleaudioplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
+	static final int SPLASH_RUNNING_TIME = 2000;
+	private Handler handler;
+	private Runnable homeActivityInvoker;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
+		handler = new Handler();
+		homeActivityInvoker = new Runnable() {
+			@Override
+			public void run() {
+				Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		};
+		handler.postDelayed(homeActivityInvoker, SPLASH_RUNNING_TIME);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		handler.removeCallbacks(homeActivityInvoker);
 	}
 }
