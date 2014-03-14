e729a86fa9f65b2b76d490ad97d02fbf657d873d
Igor Baryshnikov <masterj63@gmail.com>
2014-03-14 16:40:41 +0400

    onCreate(), onDestroy() in FragmentPlayer overriden to create and release MediaPlayer. getMediaPlayer() added

--- SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/FragmentPlayer.java
+++ SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/FragmentPlayer.java
@@ -1,7 +1,25 @@
 package mdev.master_j.simpleaudioplayer;
 
 import android.app.Fragment;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 
 public class FragmentPlayer extends Fragment {
-
+	private MediaPlayer player;
+	
+	MediaPlayer getMediaPlayer(){
+		return player;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		player = MediaPlayer.create(getActivity(), R.raw.mozart);
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		player.release();
+	}
 }
