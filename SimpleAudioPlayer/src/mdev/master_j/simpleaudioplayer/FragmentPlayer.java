bb95c1e96e9f4bdc588f596840974c436f267387
Igor Baryshnikov <masterj63@gmail.com>
2014-03-14 21:04:25 +0400

    the logic of interaction with MediaPlayer moved and hidden inside FragmentPlayer now

--- SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/FragmentPlayer.java
+++ SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/FragmentPlayer.java
@@ -6,15 +6,35 @@ import android.os.Bundle;
 
 public class FragmentPlayer extends Fragment {
 	private MediaPlayer player;
+	private boolean idle;
 	
-	MediaPlayer getMediaPlayer(){
-		return player;
+	private static final String CONTROL_BUTTON_PLAY = "Play";
+	private static final String CONTROL_BUTTON_PAUSE = "Pause";
+	
+	private static final String STATUS_TEXTVIEW_IDLE = "Idle";
+	private static final String STATUS_TEXTVIEW_PLAYING = "Playing";
+	private static final String STATUS_TEXTVIEW_PAUSED = "Paused";
+	
+	String getControlButtonText(){
+		if(player.isPlaying())
+			return CONTROL_BUTTON_PAUSE;
+		else
+			return CONTROL_BUTTON_PLAY;
+	}
+	
+	String getStatusTextViewText(){
+		if(idle)
+			return STATUS_TEXTVIEW_IDLE;
+		if(player.isPlaying())
+			return STATUS_TEXTVIEW_PLAYING;
+		return STATUS_TEXTVIEW_PAUSED;
 	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		player = MediaPlayer.create(getActivity(), R.raw.mozart);
+		idle = true;
 	}
 	
 	@Override
--- SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/MainActivity.java
+++ SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/MainActivity.java
@@ -3,7 +3,6 @@ package mdev.master_j.simpleaudioplayer;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,16 +10,9 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	private static final String FRAGMENT_PLAYER_KEY = "mdev.master_j.simpleaudioplayer.FragmentPlayer";
 	
-	private static final String CONTROL_BUTTON_PLAY = "Play";
-	private static final String CONTROL_BUTTON_PAUSE = "Pause";
-	
-	private static final String STATUS_TEXTVIEW_IDLE = "Idle";
-	private static final String STATUS_TEXTVIEW_PLAYING = "Playing";
-	private static final String STATUS_TEXTVIEW_PAUSED = "Paused";
-	
-	private MediaPlayer player;
 	private Button controlButton;
 	private TextView statusTextView;
+	private FragmentPlayer fragmentPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +23,7 @@ public class MainActivity extends Activity {
 		statusTextView = (TextView) findViewById(R.id.statusTextView);
 		
 		FragmentManager fragmentManager = getFragmentManager();
-		FragmentPlayer fragmentPlayer = (FragmentPlayer) fragmentManager.findFragmentByTag(FRAGMENT_PLAYER_KEY);
+		fragmentPlayer = (FragmentPlayer) fragmentManager.findFragmentByTag(FRAGMENT_PLAYER_KEY);
 		if(fragmentPlayer == null){
 			fragmentPlayer = new FragmentPlayer();
 			fragmentPlayer.setRetainInstance(true);
@@ -39,19 +31,18 @@ public class MainActivity extends Activity {
 			fragmentTransaction.add(fragmentPlayer, FRAGMENT_PLAYER_KEY);
 			fragmentTransaction.commit();
 		}
-		player = fragmentPlayer.getMediaPlayer();
 	}
 	
-	private void updatePlaybackStatus(boolean idle){
-		if(player.isPlaying()){
-			controlButton.setText(CONTROL_BUTTON_PAUSE);
-			statusTextView.setText(STATUS_TEXTVIEW_PLAYING);
-		}else{
-			controlButton.setText(CONTROL_BUTTON_PLAY);
-			if(idle)
-				statusTextView.setText(STATUS_TEXTVIEW_IDLE);
-			else
-				statusTextView.setText(STATUS_TEXTVIEW_PAUSED);
-		}
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if(fragmentPlayer == null)
+			fragmentPlayer = (FragmentPlayer) getFragmentManager().findFragmentByTag(FRAGMENT_PLAYER_KEY);
+		updatePlaybackStatus();
+	}
+	
+	private void updatePlaybackStatus(){
+		controlButton.setText(fragmentPlayer.getControlButtonText());
+		statusTextView.setText(fragmentPlayer.getStatusTextViewText());
 	}
 }
