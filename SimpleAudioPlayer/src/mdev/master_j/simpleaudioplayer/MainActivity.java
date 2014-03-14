e6bc2770389c4f99f80891cf1641c54626b7d252
Igor Baryshnikov <masterj63@gmail.com>
2014-03-14 16:50:53 +0400

    conditional creating of FragmentPlayer instance added. getting MediaPlayer instance from the Fragment added

--- SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/MainActivity.java
+++ SimpleAudioPlayer/src/mdev/master_j/simpleaudioplayer/MainActivity.java
@@ -1,13 +1,28 @@
 package mdev.master_j.simpleaudioplayer;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+	private static final String FRAGMENT_PLAYER_KEY = "mdev.master_j.simpleaudioplayer.FragmentPlayer";
+	private MediaPlayer player;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentPlayer fragmentPlayer = (FragmentPlayer) fragmentManager.findFragmentByTag(FRAGMENT_PLAYER_KEY);
+		if(fragmentPlayer == null){
+			fragmentPlayer = new FragmentPlayer();
+			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+			fragmentTransaction.add(fragmentPlayer, FRAGMENT_PLAYER_KEY);
+			fragmentTransaction.commit();
+		}
+		player = fragmentPlayer.getMediaPlayer();
 	}
 }
